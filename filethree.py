7e39ae34d65e13ecbe2e900cac061de7d55c9ddb
W1nters <tarnavamatvei08@gmail.com>
2024-04-11 20:37:45 +0300

    1-ое окно

---
+++ filethree.py
@@ -0,0 +1,6 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+
+class Second():
+    def __init__(self):
+        self.win = QWidget()
+        self.win.show()
--- sfrhb.py
+++ sfrhb.py
@@ -1,1 +1,23 @@
-print('ым')
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from text import *
+
+class First():
+    def __init__(self):
+        self.win = QWidget()
+        self.win.show()
+
+        self.win.setWindowTitle('Инструкция')
+        label = QLabel(description)
+        main_line = QVBoxLayout()
+        main_line.addWidget(label)
+
+        start_btn = QPushButton('Начать тест')
+        main_line.addWidget(start_btn)
+
+        self.win.setLayout(main_line)
+        self.win.setStyleSheet('font-size:28px')
+
+if __name__ == "__main__":
+    app = QApplication([])
+    win1 = First()
+    app.exec()
